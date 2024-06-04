dfa5da0bdd77edcbfb98712015de797c60b820db
Himanshu <himan9506492198@gmail.com>
2024-06-05 01:32:29 +0530

    changes in permissions

--- webscraper/settings.py
+++ webscraper/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--8ru*i^93=4ppbjw82t3jvf55c4yx2i2i58c*b1jbodcig%ih^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
