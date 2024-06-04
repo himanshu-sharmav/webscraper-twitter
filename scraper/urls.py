68dc3abe67d61082ee33d5eb5ca3a81010db812a
Himanshu <himan9506492198@gmail.com>
2024-06-05 01:46:52 +0530

    changes in static files

--- scraper/urls.py
+++ scraper/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('run_script/', views.run_script, name='run_script'),
     
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
--- webscraper/urls.py
+++ webscraper/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.contrib import staticfiles_urlpatterns
+# from django.contrib import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('scraper/', include('scraper.urls'))
 ]
-urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
